26a89f0df3e4afb1d3b97400b0bdb785f9f7fce8
pestkowiec123 <pestkowiec@gmail.com>
2014-10-21 16:13:51 +0200

    folder z baz�
    Wszystko co potrzebne do połączenia z bazą oraz kod samej bazy

---
+++ Baza Danych/baza-procedury.sql
@@ -0,0 +1,225 @@
+--/////////////////////////////////////////////////////////////////////////////////////////////////////////
+--////////////////////////////////DODAJ KLIENTA////////////////////////////////////////////////////////////
+--/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+drop procedure DODAJ_KLIENTA
+
+CREATE PROCEDURE DODAJ_KLIENTA
+(@telefon varchar(50), @email varchar(50), @kraj varchar(50), @miasto varchar(50), 
+@kod varchar(50), @ulica varchar(50), @nr_budynku varchar(50), @nr_mieszkania varchar(50), 
+@imie varchar(50), @nazwisko varchar(50), @login_uzytkownika varchar(50), @haslo varchar(50))
+as
+BEGIN
+
+BEGIN TRANSACTION a
+
+BEGIN TRY
+
+insert into DANE VALUES(@telefon, @email, NULL, @kraj, @miasto, @kod, @ulica, @nr_budynku, @nr_mieszkania, @imie, @nazwisko)
+
+declare @id INT;
+set @id = (select MAX(id_dane) from DANE );
+
+insert into KLIENT(login, id_dane, haslo) VALUES(@login_uzytkownika, @id, @haslo)
+
+END TRY
+
+BEGIN CATCH
+
+ROLLBACK TRANSACTION a
+
+END CATCH
+
+COMMIT TRANSACTION a
+
+END
+
+--przyklad uzycia
+
+EXEC DODAJ_KLIENTA
+'123456123', 'klient@gmail.com', 'Polska', 'Poznañ',
+'12-345', 'umultowska', '24A', '5',
+'Jan', 'Kowalski', 'Janek123', '12niktnieodgadnie'
+GO
+
+--/////////////////////////////////////////////////////////////////////////////////////////////////////////
+--////////////////////////////////DODAJ SPRZEDAWCE/////////////////////////////////////////////////////////
+--/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+drop procedure DODAJ_SPRZEDAWCE
+
+CREATE PROCEDURE DODAJ_SPRZEDAWCE
+(@login_uzytkownika varchar(50), @nr_konta varchar(26))
+as
+BEGIN
+
+BEGIN TRANSACTION a
+
+BEGIN TRY
+
+declare @id int;
+set @id = (select id_dane from KLIENT where login = @login_uzytkownika);
+
+UPDATE DANE
+set nr_konta = @nr_konta WHERE id_dane = @id
+
+insert into SPRZEDAWCA(login, id_dane, haslo) VALUES(@login_uzytkownika, @id, (select haslo from KLIENT where login = @login_uzytkownika))
+
+END TRY
+
+BEGIN CATCH
+
+ROLLBACK TRANSACTION a
+
+END CATCH
+
+COMMIT TRANSACTION a
+
+END
+
+--przyklad uzycia
+EXEC DODAJ_SPRZEDAWCE
+'Janek123', '12123412341234123412341234'
+GO
+
+--/////////////////////////////////////////////////////////////////////////////////////////////////////////
+--////////////////////////////////USUN KONTO///////////////////////////////////////////////////////////////
+--/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+drop procedure USUN_KONTO
+
+CREATE PROCEDURE USUN_KONTO
+(@login_uzytkownika varchar(20))
+as
+BEGIN
+
+BEGIN TRANSACTION a
+
+BEGIN TRY
+
+UPDATE KLIENT
+set data_do = GETDATE() where login = @login_uzytkownika
+
+END TRY
+
+BEGIN CATCH
+
+ROLLBACK TRANSACTION a
+
+END CATCH
+
+COMMIT TRANSACTION a
+
+BEGIN TRANSACTION b
+
+BEGIN TRY
+
+UPDATE SPRZEDAWCA
+set data_do = GETDATE() where login = @login_uzytkownika
+
+END TRY
+
+BEGIN CATCH
+
+ROLLBACK TRANSACTION b
+
+END CATCH
+
+COMMIT TRANSACTION b
+
+END
+
+--przyklad uzycia
+EXEC USUN_KONTO
+'Janek123'
+GO
+
+--/////////////////////////////////////////////////////////////////////////////////////////////////////////
+--////////////////////////////////DODAJ_AUKCJE/////////////////////////////////////////////////////////////
+--/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+drop procedure DODAJ_AUKCJE
+
+CREATE PROCEDURE DODAJ_AUKCJE
+(@kategoria varchar(20), @login_uzytkownika varchar(20), @dlugosc_trwania int, @opis text, 
+@nazwa_produktu varchar(30), @cena_startowa money, @cena_wysy³ki money)
+as
+BEGIN
+
+BEGIN TRANSACTION a
+
+BEGIN TRY
+
+declare @id int;
+set @id = (select id_kategoria from KATEGORIA where Nazwa = @kategoria);
+
+declare @data datetime;
+set @data = (select (GETDATE() + day(@dlugosc_trwania - 1)));
+
+
+INSERT INTO AUKCJA(id_kategoria, login, data_zakonczenia, opis, nazwa_produktu, cena_startowa, cena_wysylki)
+VALUES(@id, @login_uzytkownika, @data, @opis, @nazwa_produktu, @cena_startowa, @cena_wysy³ki)
+
+END TRY
+
+BEGIN CATCH
+
+ROLLBACK TRANSACTION a
+
+END CATCH
+
+COMMIT TRANSACTION a
+
+END
+
+--przyklad uzycia
+EXEC DODAJ_AUKCJE
+'AGD', 'Janek123', 3, 'lodówka z technologi¹ turbo full', 
+'beko extra lux', 300, 100 
+GO
+
+--/////////////////////////////////////////////////////////////////////////////////////////////////////////
+--////////////////////////////////DODAJ_OFERTE/////////////////////////////////////////////////////////////
+--/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+drop procedure DODAJ_OFERTE
+
+CREATE PROCEDURE DODAJ_OFERTE
+(@id_aukcji int, @login_klienta varchar(20), @kwota money)
+as
+BEGIN
+BEGIN TRANSACTION a
+	BEGIN TRY
+
+		IF((select ISNULL(MAX(kwota),0) from OFERTA where id_aukcji = @id_aukcji) <= @kwota and
+		   (select cena_startowa from AUKCJA where id_aukcji = @id_aukcji) <= @kwota)
+		BEGIN
+
+			INSERT INTO OFERTA(id_aukcji, login, kwota) 
+			VALUES(@id_aukcji, @login_klienta, @kwota)
+
+		END
+		
+		ELSE print 'podana kwota jest mniejsza od najwy¿szej oferty lub ceny startowej!'
+
+	END TRY
+
+	BEGIN CATCH
+
+		ROLLBACK TRANSACTION a
+
+	END CATCH
+
+COMMIT TRANSACTION a
+
+END
+
+--przyklad uzycia
+
+select * from AUKCJA
+
+select * from OFERTA
+
+EXEC DODAJ_OFERTE
+'3','Janek123',800
+GO
---
+++ Baza Danych/baza-tabele.sql
@@ -0,0 +1,88 @@
+drop table ZDJECIE
+drop table OFERTA
+drop table AUKCJA
+drop table KATEGORIA
+drop table SPRZEDAWCA
+drop table KLIENT
+drop table DANE
+
+CREATE TABLE DANE
+(
+	id_dane int not null IDENTITY(1,1) PRIMARY KEY,
+	telefon varchar(25) not null CHECK(telefon like '%[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]%'),
+	email varchar(50) not null CHECK(email like '%@%.%'),
+	nr_konta varchar(26) ,CHECK(nr_konta like '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
+	kraj varchar(50) not null CHECK(kraj like '[A-Z]%'),
+	miasto varchar(50) not null CHECK(miasto like '[A-Z]%'),
+	kod varchar(6) not null CHECK(kod like '[0-9][0-9]-[0-9][0-9][0-9]'),
+	ulica varchar(50) not null,
+	budynek varchar(10) not null CHECK(budynek like '[0-9]%'),
+	mieszkanie varchar(10) not null CHECK(mieszkanie like '[0-9]%'),
+	imie varchar(50) not null CHECK(imie like '[A-Z]%' and imie not like '%[0-9]%'),
+	nazwisko varchar(50) not null CHECK(nazwisko like '[A-Z]%' and nazwisko not like '%[0-9]%'),
+)
+
+CREATE TABLE SPRZEDAWCA
+(
+	login varchar(20) not null CHECK(LEN(login) >= 3) PRIMARY KEY,
+	id_dane int not null references DANE(id_dane),
+	haslo varchar(20) not null CHECK(LEN(haslo) >= 6 and haslo like '%[0-9]%'),
+	data_od datetime not null default GETDATE(), 
+	data_do datetime,
+)
+
+CREATE TABLE KLIENT
+(
+	login varchar(20) not null CHECK(LEN(login) >= 3) PRIMARY KEY,
+	id_dane int not null references DANE(id_dane),
+	haslo varchar(20) not null CHECK(LEN(haslo) >= 6 and haslo like '%[0-9]%'),
+	data_od datetime not null default GETDATE(), 
+	data_do datetime,
+)
+
+CREATE TABLE KATEGORIA
+(
+	id_kategoria int not null IDENTITY(1,1) PRIMARY KEY,
+	id_rodzica int references KATEGORIA(id_kategoria),
+	Nazwa varchar(20) not null,
+	czy_lisc bit, --1 - true, 0 - false
+)
+
+insert into KATEGORIA VALUES(NULL, 'wszystkie', 0)
+insert into KATEGORIA VALUES(1, 'RTV', 0)
+insert into KATEGORIA VALUES(1, 'AGD', 0)
+insert into KATEGORIA VALUES(1, 'sport', 0)
+insert into KATEGORIA VALUES(1, 'komputery', 0)
+
+CREATE TABLE AUKCJA 
+(
+	id_aukcji int not null IDENTITY(1,1) PRIMARY KEY,
+	id_kategoria int not null references KATEGORIA(id_kategoria),
+	login varchar(20) not null references SPRZEDAWCA(login),
+	data_zakonczenia datetime not null,
+	opis text not null,
+	nazwa_produktu varchar(30) not null,
+	cena_startowa money not null,
+	cena_wysylki money not null,
+	ocena_sprzedawcy int CHECK(ocena_sprzedawcy <= 10),
+	komentarz_dla_sprzedawcy text,
+)
+
+CREATE TABLE ZDJECIE
+(
+  id_zdjecia int not null IDENTITY(1,1) PRIMARY KEY,
+  id_aukcji int not null references AUKCJA(id_aukcji),
+  zdjecie image not null,
+  podpis varchar(50),
+)
+
+CREATE TABLE OFERTA 
+(
+  id_oferta int not null IDENTITY(1,1) PRIMARY KEY,
+  id_aukcji int not null references AUKCJA(id_aukcji),
+  login varchar(20) not null references KLIENT(login),
+  kwota money not null,
+  data_zlozenia datetime not null default GETDATE(), 
+)
+
+
