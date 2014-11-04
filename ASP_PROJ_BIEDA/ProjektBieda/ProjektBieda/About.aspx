9e822f2662bc1960a99d05fd5232a493fb2ab70f
kojotek <dargemir@wp.pl>
2014-11-04 01:05:45 +0100

    Działające połączenie z bazą danych w About.aspx

--- ASP_PROJ_BIEDA/ProjektBieda/ProjektBieda/About.aspx
+++ ASP_PROJ_BIEDA/ProjektBieda/ProjektBieda/About.aspx
@@ -1,7 +1,9 @@
 ﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="ProjektBieda.About" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-    <h2><%: Title %>.</h2>
-    <h3>Your application description page.</h3>
-    <p>Use this area to provide additional information.</p>
+    <br /><br /><br />
+    <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label><br />
+    <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label><br />
+    <asp:Label ID="Label3" runat="server" Text="Label"></asp:Label><br />
+    <asp:Label ID="Label4" runat="server" Text="Label"></asp:Label><br />
 </asp:Content>
--- ASP_PROJ_BIEDA/ProjektBieda/ProjektBieda/About.aspx.cs
+++ ASP_PROJ_BIEDA/ProjektBieda/ProjektBieda/About.aspx.cs
@@ -6,15 +6,49 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 using System.Data.Sql;
+using System.Data;
 
 namespace ProjektBieda
 {
     public partial class About : Page
     {
+
+        public void SqlConnect(string serverName, string database, string userId, string password)
+        {
+            SqlConnection conn = null; SqlCommand cmd = null;
+
+            try { conn = new SqlConnection("Server=" + serverName + ";Database=" + database + ";User Id=" + userId + ";Password=" + password + ";"); conn.Open(); }
+            catch (InvalidOperationException ex) { System.Web.HttpContext.Current.Response.Write("<SCRIPT LANGUAGE=\"\"JavaScript\"\">alert(\"Klient\")</SCRIPT>"); }
+            catch (SqlException ex) { System.Web.HttpContext.Current.Response.Write("<SCRIPT LANGUAGE=\"\"JavaScript\"\">alert(\"serwer!\")</SCRIPT>"); }
+            finally
+            {
+                try
+                { 
+                    cmd = new SqlCommand("select * from dane", conn);
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        Label1.Text = dr[1].ToString();
+                        Label2.Text = dr[2].ToString();
+                        Label3.Text = dr[3].ToString();
+                        Label4.Text = dr[4].ToString();
+                    }
+                } 
+                catch (Exception) 
+                {
+                    System.Web.HttpContext.Current.Response.Write("<SCRIPT LANGUAGE=\"\"JavaScript\"\">alert(\"dupa2!\")</SCRIPT>");
+                } 
+                finally 
+                {
+                    System.Web.HttpContext.Current.Response.Write("<SCRIPT LANGUAGE=\"\"JavaScript\"\">alert(\"hahaha!\")</SCRIPT>");
+                }
+            }
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
-            //SqlConnection conn = new SqlConnection("server=;password=;user_id=;ingrated_security=true;");
+            SqlConnect("mssql.wmi.amu.edu.pl", "dtas_s383964", "s383964", "674lCgcV");
 
         }
     }
