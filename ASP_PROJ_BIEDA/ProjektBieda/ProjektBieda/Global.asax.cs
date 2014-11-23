8bb48ca599095f4895821bda7d65cb5920135f0b
pestkowiec123 <pestkowiec@gmail.com>
2014-11-23 18:52:03 +0100

    Pełny system logowania

--- ASP_PROJ_BIEDA/ProjektBieda/ProjektBieda/Global.asax.cs
+++ ASP_PROJ_BIEDA/ProjektBieda/ProjektBieda/Global.asax.cs
@@ -17,5 +17,12 @@ namespace ProjektBieda
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+        void Session_Start(object sender, EventArgs e)
+        {
+            // Code that runs when a new session is started
+
+            Session["zalogowand"] = null;
+        }
     }
 }
