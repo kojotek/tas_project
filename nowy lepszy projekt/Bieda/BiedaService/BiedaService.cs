8e2ca88a0a2f354f289b8e3c142811baa23dcc94
kojotek <dargemir@wp.pl>
2014-12-13 17:49:59 +0100

    Moje paranoje i dywagacje, nie ruszać

---
+++ nowy lepszy projekt/Bieda/BiedaService/BiedaService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWCFServices
+{
+    public class BiedaService: IBiedaService
+    {
+        public String GetMessage(String name)
+        {
+            return "Kojotki witaja, " + name + "! Czuj sie jak u siebie!";
+        }
+    }
+}
---
+++ nowy lepszy projekt/Bieda/BiedaService/IBiedaService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace MyWCFServices
+{
+    [ServiceContract]
+    public interface IBiedaService
+    {
+        [OperationContract]
+        String GetMessage(String name);
+    }
+}
